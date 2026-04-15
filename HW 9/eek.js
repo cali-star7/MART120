f9a51c16d50d018be14a5359c6a2a5d09b7dc243
cali-star7 <cs231295@umconnect.umt.edu>
2026-04-14 18:56:25 -0600

    hw 9

---
+++ HW 9/eek.js
@@ -0,0 +1,49 @@
+function setup ( ){
+    createCanvas(400,400);
+}
+function draw () {
+    background(245, 241, 177);
+    noStroke();
+    textSize(12);
+    fill(200, 0, 0,)
+    text("x: " + mouseX + ", y: " + mouseY, 10, 20);
+
+      fill(69, 41, 0);
+    rect(118,192,160,160);
+    circle(194,186,180);
+    fill(219, 136, 235);
+    ellipse(186,360,150,200);
+    fill (250, 211, 153);
+    rect(164,251,50,30);
+    triangle(164,280,213,280,186,308);
+    circle(120,197,20);
+    circle(265,197,20);
+    circle (194,186,150);
+    fill(0,0,0);
+    ellipse (155,175,60,20);
+    ellipse (233,175,60,20);
+    fill (247, 247, 247);
+    ellipse (160,180,50,20);
+    ellipse (228,180,50,20);
+    fill (0,0,0);
+    circle(159,179,18);
+    circle (228,179,18);
+    fill(252, 239, 219);
+    triangle (184,201,206,201,193,187);
+    fill(0,0,0);
+    line(169,219,212,219);
+rect(177,219,30,5);
+fill(69,41,0);
+rect(131,159,50,2);
+rect(206,159,50,2);
+text('Cali',178,347);
+text ('this is totally me',191,56)
+point (248,194);
+
+   
+
+
+}
+
+
+
